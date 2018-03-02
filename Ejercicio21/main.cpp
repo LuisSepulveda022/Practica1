17b53387f86c0b76fdd5b6afbfd1fcc0c9d1f346
LuisSepulveda022 <luis.sepulveda1@udea.edu.co>
2018-03-02 11:00:31 -0500

    Ejercicio21

---
+++ Ejercicio21/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+/*
+ * Escriba un programa que pida un carácter C, si es una letra la debe convertir de
+mayúscula a minúscula y viceversa e imprimirla.
+Ej: si se ingresa B se debe imprimir:
+Letra convertida: b
+y si se ingresa k se debe imprimir:
+Letra convertida: K*/
+int main()
+{
+       char c;      // definimos la variable
+       cout << "Ingrese una letra: ";  //pedimos el valor para la variable por consola
+       cin >> c;    // almacenamos el dato ingresadado por consola en la variable
+
+       if  (int(c) < 91){       //convertimos con el int(c) la letra en numero y miramos si la letra esta en las mayusculas de la tabla ASCII
+
+           cout << char(c+32)<< endl;  //imprimos char(c+32) que es el numero de la letra + 32 para que sea ya minuscula y la convertimos en letra
+
+       }else if( int(c) > 96){  //convertimos con el int(c) la letra en numero y miramos si la letra esta en las minusculas de la tabla ASCII
+
+           cout << char(c-32)<<endl; //imprimos char(c-32) que es el numero de la letra - 32 para que sea ya mayuscula y la convertimos en letra
+        }
+
+    return 0;
+}
