ab2db2d9d54f4da96cef20fc494865c1de7062e6
Luis Sepulveda <luis.sepulveda1@udea.edu.co>
2018-03-07 15:49:40 -0500

    Ejercicio3.1

--- Ejercicio3/main.cpp
+++ Ejercicio3/main.cpp
@@ -10,17 +10,19 @@ int main()
 {
 
     int numero1,numero2;
-    cout << "Ingrese el primer Número: ";
+    cout << "Ingrese el primer Numero: ";
     cin >> numero1; //Pedimos por consola el número 1
-    cout << "Ingrese el segundo Número: ";
+    cout << "Ingrese el segundo Numero: ";
     cin >> numero2; //Pedimos por consola el número 2
 
     if(numero1>numero2){ // Evaluamos si el número 1 es mayor a el número 2
-        cout<<"El mayor es el Número 1: "<<numero1<< endl;
+        cout<<"El mayor es el Numero 1: "<<numero1<< endl;
 
     }
-    else{
-        cout<<"El mayor es el Número 2: "<<numero2<< endl;
+    else if(numero2>numero1){
+        cout<<"El mayor es el Numero 2: "<<numero2<< endl;
+    }else{
+        cout<<"Los numeros ingresados son iguales."<<endl; // si ninguna de las dos condiciones se cumple es porque los numeros son iguales
     }
     return 0;
 }
